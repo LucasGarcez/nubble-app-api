99fa255231e31275fe4a5a11e0e1c5aac16372bf
EmersonGarrido <emersongarrido@hotmail.com.br>
2023-07-03 08:50:34 -0400

    add new format seed posts

--- database/seeders/02Post.ts
+++ database/seeders/02Post.ts
@@ -5,7 +5,7 @@ export default class UserSeeder extends BaseSeeder {
   public async run() {
     await Post.createMany([
       {
-        id: 1,
+        user_id: 1,
         text: 'This is my first post!',
         author: {
           profileURL: 'https://i.imgur.com/YeE3yKf.jpg',
@@ -15,7 +15,7 @@ export default class UserSeeder extends BaseSeeder {
         imageUrl: 'https://i.imgur.com/VyfuWYa.jpg',
       },
       {
-        id: 1,
+        user_id: 1,
         text: 'Check out this cool photo!',
         author: {
           profileURL: 'https://i.imgur.com/BrJUnRl.jpg',
@@ -25,7 +25,7 @@ export default class UserSeeder extends BaseSeeder {
         imageUrl: 'https://i.imgur.com/KkcrB5y.jpg',
       },
       {
-        id: 1,
+        user_id: 1,
         text: "Just finished reading this book and it's amazing!",
         author: {
           profileURL: 'https://i.imgur.com/BxgHDci.jpg',
@@ -35,7 +35,7 @@ export default class UserSeeder extends BaseSeeder {
         imageUrl: 'https://i.imgur.com/NdrA2BY.jpg',
       },
       {
-        id: 2,
+        user_id: 2,
         text: "I can't believe it's already May!",
         author: {
           profileURL: 'https://i.imgur.com/FmSbPv3.jpg',
@@ -45,7 +45,7 @@ export default class UserSeeder extends BaseSeeder {
         imageUrl: 'https://i.imgur.com/vkscdqk.jpg',
       },
       {
-        id: 2,
+        user_id: 2,
         text: 'Coding day!',
         author: {
           profileURL: 'https://i.imgur.com/DF4Jfxq.jpg',
@@ -55,7 +55,7 @@ export default class UserSeeder extends BaseSeeder {
         imageUrl: 'https://i.imgur.com/rsOe2hC.jpg',
       },
       {
-        id: 2,
+        user_id: 2,
         text: "What's your favorite color?",
         author: {
           profileURL: 'https://i.imgur.com/YeE3yKf.jpg',
@@ -65,7 +65,7 @@ export default class UserSeeder extends BaseSeeder {
         imageUrl: 'https://i.imgur.com/zx94i19.jpg',
       },
       {
-        id: 3,
+        user_id: 3,
         text: "I'm so excited for the weekend!",
         author: {
           profileURL: 'https://i.imgur.com/BrJUnRl.jpg',
@@ -75,7 +75,7 @@ export default class UserSeeder extends BaseSeeder {
         imageUrl: 'https://i.imgur.com/LWopzZH.jpg',
       },
       {
-        id: 3,
+        user_id: 3,
         text: 'Just got back from vacation and it was amazing!',
         author: {
           profileURL: 'https://i.imgur.com/BxgHDci.jpg',
@@ -85,7 +85,7 @@ export default class UserSeeder extends BaseSeeder {
         imageUrl: 'https://i.imgur.com/WZZLeHg.jpg',
       },
       {
-        id: 3,
+        user_id: 3,
         text: "I'm loving this new restaurant!",
         imageUrl: 'https://i.imgur.com/FmSbPv3.jpg',
         author: {
